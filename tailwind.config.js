f039e378db86945e5cc76e32155d14c4a725e9d1
unknown <kcavner@hotmail.com>
2023-09-25 22:31:43 -0500

    added custom colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor:{
+        'vintage-white': '#F1F0E8',
+        'vintage-tan': '#eee0c9',
+        'vintage-blue': '#ADC4CE',
+        'vintage-blue2': '#96B6C5'
+      },
+      textColor:{
+        'vintage-white': '#F1F0E8',
+        'vintage-blue': '#ADC4CE'
+      }
+    },
   },
   plugins: [],
 }
