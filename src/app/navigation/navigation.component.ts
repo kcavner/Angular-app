6034584fe679f1f3de9b15d7ad24c3884d860fa0
unknown <kcavner@hotmail.com>
2023-09-29 13:31:45 -0500

    added folder bar

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -11,23 +11,32 @@ export class NavigationComponent {
   barSvg = "/assets/svg/barSolid.svg"
   staggeredBarSvg = "/assets/svg/bars-staggered-solid.svg"
   fileSvg = "/assets/svg/file-regular.svg"
-  openFolderSvg = "/assets/svg/folder-open.svg"
+  openFolderSvg = "/assets/svg/folder-open-regular.svg"
   folderSvg = "/assets/svg/folder-regular.svg"
 
   shouldApplyClass: boolean = true;
+  shouldApplyFolderClass: boolean = true;
   currentBarSvg: string = this.barSvg
+  currentFolderSvg: string = this.folderSvg
 
   getDynamicClass(): string{
       return this.shouldApplyClass ? 'hidden' : '';
   }
 
-  toggleSvg() {
+  toggleBarSvg() {
     this.currentBarSvg = this.currentBarSvg === this.barSvg ? this.staggeredBarSvg : this.barSvg;
     this.toggleClass()
+  }
+  toggleFolderSvg() {
+    this.currentFolderSvg = this.currentFolderSvg === this.folderSvg ? this.openFolderSvg : this.folderSvg;
+    this.toggleFolderClass()
   }
 
   toggleClass() {
     this.shouldApplyClass = !this.shouldApplyClass;
+  }
+  toggleFolderClass() {
+    this.shouldApplyFolderClass = !this.shouldApplyFolderClass;
   }
 
 }
