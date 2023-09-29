ddab7c978c5f2fea1bf568450386df7aa367d35f
unknown <kcavner@hotmail.com>
2023-09-29 12:39:29 -0500

    toggle svg

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
-  shouldApplyClass: boolean = true;
 
+export class NavigationComponent {
+  barSvg = "/assets/svg/barSolid.svg"
+  staggeredBarSvg = "/assets/svg/bars-staggered-solid.svg"
+  fileSvg = "/assets/svg/file-regular.svg"
+  openFolderSvg = "/assets/svg/folder-open.svg"
+  folderSvg = "/assets/svg/folder-regular.svg"
+
+  shouldApplyClass: boolean = true;
+  currentBarSvg: string = this.barSvg
 
   getDynamicClass(): string{
-
-      // Your condition here
       return this.shouldApplyClass ? 'hidden' : '';
+  }
+
+  toggleSvg() {
+    this.currentBarSvg = this.currentBarSvg === this.barSvg ? this.staggeredBarSvg : this.barSvg;
   }
 
   toggleClass() {
     this.shouldApplyClass = !this.shouldApplyClass;
   }
-
 
 }
