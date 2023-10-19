b12ca557b76e018c7131767a69837595c0c47d86
unknown <kcavner@hotmail.com>
2023-10-18 19:31:47 -0500

    added note component section

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoteCreatorComponent } from './note-creator/note-creator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'notes',component:NoteCreatorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
