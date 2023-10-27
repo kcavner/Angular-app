a35e353ab2f6bd2fcf132923cc77707eef0f44b2
unknown <kcavner@hotmail.com>
2023-10-26 23:30:45 -0500

    added tailwind styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,16 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
     extend: {
-      backgroundColor:{
-        'vintage-white': '#F1F0E8',
-        'vintage-tan': '#eee0c9',
-        'vintage-blue': '#ADC4CE',
-        'vintage-blue2': '#96B6C5'
-      },
-      textColor:{
-        'vintage-white': '#F1F0E8',
-        'vintage-blue': '#ADC4CE'
-      }
+
     },
   },
   plugins: [],
